b24061685762d9cdd79c48e03757eb8fb06b74f1
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2023-04-24 18:20:52 +0100

    Update Remediation.ps1
    Updated to call the MSIs directly as cabs aren't included in the latest version. Thanks to https://github.com/chrisnelmes for the contribution.

--- Microsoft Update Health Tools/Remediation.ps1
+++ Microsoft Update Health Tools/Remediation.ps1
@@ -24,37 +24,44 @@ else
 
 # Determine which cab to use
 [int]$CurrentBuild = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "CurrentBuildNumber" | Select -ExpandProperty CurrentBuildNumber
-if ($CurrentBuild -ge 22000)
+if ($CurrentBuild -gt 22000)
 {
-    $OSVersion = "Windows11"
+    $OSVersion = "Windows 11 22H2+"
+}
+elseif ($CurrentBuild -eq 22000) {
+    $OSVersion = "Windows 11 21H2"
 }
 else 
 {
-    $OSVersion = "Windows10"    
+    $OSVersion = "Windows 10"    
 }
-[string]$Bitness = Get-CimInstance Win32_OperatingSystem -Property OSArchitecture | Select -ExpandProperty OSArchitecture
-Switch ($Bitness)
-{
-    "64-bit" {$arch = "x64"}
-    "32-bit" {$arch = "x86"}
-    default { Write-Host "Unable to determine OS architecture"; Exit 1 }
-}
-If ($CurrentBuild -eq 22621)
-{
-    $CabLocation = "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\$OSVersion\22H2\$arch"   
-}
-else 
-{
-    $CabLocation = "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\$OSVersion\$arch"     
-}
-$CabName = (Get-ChildItem $CabLocation -Name *.cab).pschildname
+#[string]$Bitness = Get-CimInstance Win32_OperatingSystem -Property OSArchitecture | Select -ExpandProperty OSArchitecture
+#Switch ($Bitness)
+#{
+#    "64-bit" {$arch = "x64"}
+#    "32-bit" {$arch = "x86"}
+#    default { Write-Host "Unable to determine OS architecture"; Exit 1 }
+#}
+#If ($CurrentBuild -eq 22621)
+#{
+#    $CabLocation = "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\$OSVersion\22H2\$arch"   
+#}
+#else 
+#{
+#    $CabLocation = "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\$OSVersion\$arch"     
+#}
+#$CabName = (Get-ChildItem $CabLocation -Name *.cab).pschildname
 
 # Expand the cab and get the MSI
-expand.exe /r "$CabLocation\$CabName" /F:* "$DownloadDirectory\$($DownloadFileName.Split('.')[0])"
-Start-Sleep -Seconds 5
-expand.exe /r "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\UpdHealthTools.cab" /F:* "$DownloadDirectory\$($DownloadFileName.Split('.')[0])"
-Start-Sleep -Seconds 5
-$File = Get-Childitem -Path "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\*.msi" -File
+#expand.exe /r "$CabLocation\$CabName" /F:* "$DownloadDirectory\$($DownloadFileName.Split('.')[0])"
+#Start-Sleep -Seconds 5
+#expand.exe /r "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\UpdHealthTools.cab" /F:* "$DownloadDirectory\$($DownloadFileName.Split('.')[0])"
+#Start-Sleep -Seconds 5
+#$File = Get-Childitem -Path "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\*.msi" -File
+
+# Get MSI file to use
+$MsiLocation = "$DownloadDirectory\$($DownloadFileName.Split('.')[0])\$OSVersion"   
+$File = Get-Childitem -Path "$MsiLocation\*.msi" -File
 
 # Install the MSI
 $Process = Start-Process -FilePath msiexec.exe -ArgumentList "/i $($File.FullName) /qn REBOOT=ReallySuppress /L*V ""$LogDirectory\$LogFile""" -Wait -PassThru
